61bae743a50a60cc28dad385401a1922cd28c667
Гамидов Шамсудин Селимханович <shsgamidov@edu.hse.ru>
2022-07-25 19:04:05 +0300

    add modal + API query + decompose

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
+import PostService from "./API/PostService";
+import { useFetching } from "./components/hooks/useFetching";
+import { usePosts } from "./components/hooks/usePosts";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 // import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyLoader from "./components/UI/loader/MyLoader";
+import MyModal from "./components/UI/modal/MyModal";
 import './styles/App.css'
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: "JavaScript", body: "Веб-разработка"},
-    {id: 2, title: "Python", body: "Машинное обучение"},
-    {id: 3, title: "C++", body: "Игровые движки"}
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  })
 
-  const [filter, setFilter] = useState({sort: '', query: ''})
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   // Получаем newPost из дочернего элемента
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   // Получаем post из дочернего элемента
@@ -36,11 +39,19 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-
   return (
     <div className="App">
-      <h1 style={{textAlign: "center"}}>Создание постов</h1>
-      <PostForm create={createPost} />
+      <MyButton 
+        style={{marginTop: 30}}
+        onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+
+      <MyModal visible={modal} setVisible={setModal}>
+        <h1 style={{textAlign: "center"}}>Создание постов</h1>
+        <PostForm create={createPost} />
+      </MyModal>
+      
       <hr style={{margin: "15px 0"}} />
 
       <h1 style={{textAlign: "center"}}>Поиск постов</h1>
@@ -48,12 +59,19 @@ function App() {
         filter={filter} 
         setFilter={setFilter} 
       />
+      {postError && 
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><MyLoader /></div>
+        : <PostList 
+            remove={removePost} 
+            posts={sortedAndSearchedPosts} 
+            title="Посты по программированию" 
+          /> 
+      }
 
-      <PostList 
-        remove={removePost} 
-        posts={sortedAndSearchedPosts} 
-        title="Посты про JS" 
-      /> 
+      
     </div>
   );
 }
